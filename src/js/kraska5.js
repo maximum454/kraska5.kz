9a4564b4fe1c32bf61c2d64653e341fc1acad18d
maxim <karachyov@musan.kz>
2018-06-20 16:08:12 +0600

    header footer contacts

--- src/js/kraska5.js
+++ src/js/kraska5.js
@@ -15,22 +15,6 @@ $(function () {
     });
 
 
-    $('.popap_box').click(function () {
-        $('#modalbox_' + $(this).data('body')).arcticmodal({
-            overlay: {
-                css: {
-                    backgroundColor: '#000'
-                }
-            }
-        });
-    });
-    $("#menu").on("click", "a", function (event) {
-        event.preventDefault();
-        var id = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top - 50}, 1500);
-    });
-
     $('.js-now').slick({
         autoplay: false,
         dots: false,
